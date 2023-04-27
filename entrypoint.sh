132ba5e584890dafc47a863796e16ea92c3f6fd3
RSereno <rafaelsereno@gmail.com>
2023-04-27 21:21:27 +0100

    Update and rename check-version.sh to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,7 @@ if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
 fi
 
 # Start BedrockConnect
-echo "Starting BedrockConnect container..."
+echo "[$(date)] Starting  $CONTAINER_NAME container...with the following variables:\" && echo \"useDB=$useDB\" && echo \"featured_servers=$featured_servers\" && echo \"allowUIServerManagement=false\n\n\"
 java -Xms512M -Xmx512M -jar BedrockConnect-*.jar nodb=true user_servers=false custom_servers=/app/config/custom_servers.json language=/app/config/servername.json featured_servers=$FEATURED_SERVERS
+
+echo "BedrockConnect container stopped."

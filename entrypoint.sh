5c5a1e073be8613599d421c5518ded9f2e414d7a
RSereno <rafaelsereno@gmail.com>
2023-06-08 18:52:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Start BedrockConnect
-echo "[$(date)] Starting  $CONTAINER_NAME container...with the following variables:\" && echo \"useDB=$useDB\" && echo \"featured_servers=$featured_servers\" && echo \"allowUIServerManagement=false\n\n\"
+echo "[$(date)] Starting  $CONTAINER_NAME container...with the following variables:\"" && 
+echo \"useDB=$useDB\" && 
+echo \"featured_servers=$featured_servers\" && echo \"allowUIServerManagement=false\n\n\"
 java -Xms512M -Xmx512M -jar bedrock-connect.jar nodb=true user_servers=false custom_servers=/app/config/custom_servers.json language=/app/config/servername.json featured_servers=$FEATURED_SERVERS"
 
 echo "BedrockConnect container stopped."
